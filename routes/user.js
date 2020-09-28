754a2c5ea84cfcdaea71f57069a06a82b2116123
thswpvm <sonjeff@naver.com>
2020-09-28 14:23:58 +0900

    make nodejs post for login and signup

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,32 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (Req, res) => {
-  res.end("adsf");
+const { login, signup } = require("../api/mysql");
+const Schema = require("../model/user");
+const Response = require("../response");
+
+router.post("/login", (req, res) => {
+  const errors = Schema.user.validate(req.body);
+
+  if (errors.length != 0) res.status(400).json(Response.fail(errors[0]));
+  else {
+    const { username, password } = req.body;
+    login(username, password)
+      .then((id) => res.json(Response.success({ id: id })))
+      .catch((err) => res.status(400).json(Response.fail(err)));
+  }
+});
+
+router.post("/signup", (req, res) => {
+  const errors = Schema.user.validate(req.body);
+
+  if (errors.length != 0) res.status(400).json(Response.fail(errors[0]));
+  else {
+    const { username } = req.body.username;
+    signup(req.body)
+      .then((id) => res.json(Response.success({ username: username, id: id })))
+      .catch((err) => res.status(400).json(Response.fail(err)));
+  }
 });
 
 module.exports = router;
