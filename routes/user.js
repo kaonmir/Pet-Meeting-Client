a82b33fd565d3a177dbd951a449d57d13e1e61d5
thswpvm <sonjeff@naver.com>
2020-09-29 00:41:31 +0900

    make session module and configure it

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,18 @@ const { login, signup } = require("../api/mysql");
 const Schema = require("../model/user");
 const Response = require("../response");
 
+router.get("/id", (req, res) => {
+  const { uid } = req.session;
+  if (uid) res.json(Response.success({ id: uid }));
+  else res.status(400).json(Response.fail("Not logined"));
+});
+
+router.get("/logined", (req, res) => {
+  const { uid } = req.session;
+  if (uid) res.json(Response.success({ id: uid }));
+  else res.status(400).json(Response.fail("Not logined"));
+});
+
 router.post("/login", (req, res) => {
   console.log(req.body);
   const errors = Schema.user.validate(req.body);
@@ -15,13 +27,19 @@ router.post("/login", (req, res) => {
   else {
     const { username, password } = req.body;
     login(username, password)
-      .then((id) => res.json(Response.success({ id: id })))
+      .then((id) => {
+        req.session.uid = id; // Set session's id
+        res.json(Response.success({ id: id }));
+      })
       .catch((err) => res.status(400).json(Response.fail(err)));
   }
 });
 
 router.post("/signup", (req, res) => {
   const errors = Schema.user.validate(req.body);
+
+  console.log(req.body);
+  req.json;
 
   if (errors.length != 0) res.status(400).json(Response.fail(errors[0]));
   else {
@@ -30,6 +48,13 @@ router.post("/signup", (req, res) => {
       .then((id) => res.json(Response.success({ username: username, id: id })))
       .catch((err) => res.status(400).json(Response.fail(err)));
   }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) res.status(400).json(Response.fail(err));
+    else res.json(Response.success({}));
+  });
 });
 
 module.exports = router;
