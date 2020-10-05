c8fbd3248ed62e0561d76c41ba3af67359870fc3
thswpvm <sonjeff@naver.com>
2020-10-05 09:13:24 +0900

    showoff best page

--- api/showoff.js
+++ api/showoff.js
@@ -1,17 +1,24 @@
 const Connection = require("./mysql");
 
 module.exports = {
-  list: (limit, offset) =>
+  best: () =>
     new Promise((reslove, reject) => {
       // Later, 저거 명령문 해킹 방지하기
 
-      const sql = `SELECT s.Title, s.Text, s.ImgUrl, u.Username FROM petmeeting.Showoff AS s
-                  JOIN petmeeting.User AS u ON s.UID = u.UID ORDER BY s.Date DESC LIMIT ${limit} OFFSET ${offset}`;
+      const _m = new Date();
+      const date = `${_m.getFullYear()}-${_m.getMonth()}-${_m.getDate()}`;
+      console.log(date);
+      const sql = `SELECT s.ImgUrl, s.Text, s.Date, COUNT(v.UID) AS Votes, u.Username
+                    FROM petmeeting.Showoff AS s
+                    LEFT JOIN petmeeting.Vote AS v ON s.SID = v.SID
+                    JOIN petmeeting.User AS u ON s.UID = u.UID
+                    WHERE s.Date > str_to_date('${date}', '%Y-%m-%d')
+                    GROUP BY s.SID ORDER BY Votes DESC LIMIT 1`;
       Connection.get().query(sql, (err, rows) => {
         if (err) reject(err);
         else {
           console.log(rows);
-          reslove(rows);
+          reslove(rows[0]);
         }
       });
     }),
--- routes/showoff.js
+++ routes/showoff.js
@@ -7,16 +7,13 @@ router.get("/", (req, res) => {
   const limit = req.query.limit | 5;
   const offset = req.query.offset | 0;
 
-  if (limit < 0 || offset < 0)
-    res.status(400).json(response.fail("Parameter Error"));
-  else
-    worries
-      .list(limit, offset)
-      .then((result) => res.json(response.success(result)))
-      .catch((err) => {
-        console.log(err);
-        res.json(response.fail("Database Error"));
-      });
+  showoff
+    .best()
+    .then((result) => res.json(response.success(result)))
+    .catch((err) => {
+      console.log(err);
+      res.json(response.fail("Database Error"));
+    });
 });
 
 module.exports = router;
