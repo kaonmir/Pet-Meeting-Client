c91504849f741813fa500fdc6f3cc7212924175d
thswpvm <sonjeff@naver.com>
2020-10-05 11:59:18 +0900

    DEV. get, best the showoffs

--- api/showoff.js
+++ api/showoff.js
@@ -2,12 +2,13 @@ const Connection = require("./mysql");
 
 module.exports = {
   best: () =>
-    new Promise((reslove, reject) => {
+    new Promise((resolve, reject) => {
       // Later, 저거 명령문 해킹 방지하기
 
       const _m = new Date();
-      const date = `${_m.getFullYear()}-${_m.getMonth()}-${_m.getDate()}`;
-      console.log(date);
+      const month = _m.getMonth() != 0 ? _m.getMonth() : 12; // 한 달 빼주기
+      const date = `${_m.getFullYear()}-${month}-${_m.getDate()}`;
+
       const sql = `SELECT s.ImgUrl, s.Text, s.Date, COUNT(v.UID) AS Votes, u.Username
                     FROM petmeeting.Showoff AS s
                     LEFT JOIN petmeeting.Vote AS v ON s.SID = v.SID
@@ -16,10 +17,20 @@ module.exports = {
                     GROUP BY s.SID ORDER BY Votes DESC LIMIT 1`;
       Connection.get().query(sql, (err, rows) => {
         if (err) reject(err);
-        else {
-          console.log(rows);
-          reslove(rows[0]);
-        }
+        else resolve(rows[0]);
+      });
+    }),
+  list: (limit, offset) =>
+    new Promise((resolve, reject) => {
+      // Later, 저거 명령문 해킹 방지하기
+
+      const sql = `SELECT s.SID, s.ImgUrl, s.Text, s.Date, u.Username
+                    FROM petmeeting.Showoff AS s 
+                    JOIN petmeeting.User AS u ON s.UID = u.UID
+                    ORDER BY s.Date DESC LIMIT ${limit} OFFSET ${offset}`;
+      Connection.get().query(sql, (err, rows) => {
+        if (err) reject(err);
+        else resolve(rows);
       });
     }),
 };
--- routes/showoff.js
+++ routes/showoff.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const showoff = require("../api/showoff");
 const response = require("../response");
+const session = require("../services/session");
+const { formatTime } = require("../services/format");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const limit = req.query.limit | 5;
-  const offset = req.query.offset | 0;
+  const limit = req.query.limit || 5;
+  const offset = req.query.offset || 0;
 
+  showoff
+    .list(limit, offset)
+    .then((result) => res.json(response.success(result)))
+    .catch((err) => res.json(response.fail("Database Error")));
+});
+
+router.get("/best", (req, res) => {
   showoff
     .best()
     .then((result) => res.json(response.success(result)))
-    .catch((err) => {
-      console.log(err);
-      res.json(response.fail("Database Error"));
-    });
+    .catch((err) => res.json(response.fail("Database Error")));
 });
 
 module.exports = router;
