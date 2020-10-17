78aea0e0418715877fd6e22634eaf65923e4ca68
KAONMIR <40649016+kaonmir@users.noreply.github.com>
2020-10-17 20:00:18 +0900

    Feat#18 application (#32)
    * FIX Change DB table
    
    * DEV. Big Change!!
    
    * Big change!
    
    * commit
    
    * commit

--- model/entrust.js
+++ model/entrust.js
@@ -8,12 +8,12 @@ const schema = new Schema({
   StartDate: {
     type: String,
     required: true,
-    match: /^\d{4}-\d{2}-\d{2}$/,
+    match: /^ *\d{4}-\d{2}-\d{2} *$/,
   },
   EndDate: {
     type: String,
     required: true,
-    match: /^\d{4}-\d{2}-\d{2}$/,
+    match: /^ *\d{4}-\d{2}-\d{2} *$/,
   },
   Preypayment: {
     type: Boolean,
