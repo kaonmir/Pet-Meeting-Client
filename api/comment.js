dcb366ca046673a0ac92bdf20c9aa491fd4f241d
thswpvm <sonjeff@naver.com>
2020-11-04 06:39:41 +0900

    change more

--- api/comment.js
+++
@@ -1,9 +0,0 @@
-const MySQL = require("./mysql");
-
-module.exports = {
-  list: (wid, limit, offset) => {
-    const sql = `SELECT * FROM petmeeting.CommentView WHERE WID='${wid}' 
-                  LIMIT ${limit} OFFSET ${offset}`;
-    return MySQL.query(sql);
-  },
-};
